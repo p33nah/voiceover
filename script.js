b2dc9a08d15cff3a1fd8eefe64c2c8ba1b742edc
Andrea Pinardi <andreapinardi92@gmail.com>
2022-07-07 13:43:52 +0200

    test trap

--- script.js
+++ script.js
@@ -4,7 +4,14 @@ const close = document.querySelector(".close");
 
 open.onclick = () => {
   menu.style.display = "block";
+  close.focus();
 };
 close.onclick = () => {
   menu.style.display = "none";
 };
+
+document.addEventListener("focus", (e) => {
+  if (menu.style.display === "block" && !menu.contains(e.target)) {
+    console.log("focus");
+  }
+});
